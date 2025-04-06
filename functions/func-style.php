7082da8d070a8a0aa1d7ac04269445f7eeada457
SarahS0307 <sarah.schoenthaler@web.de>
2025-04-06 18:29:34 +0200

    safe tailwind setup

--- functions/func-style.php
+++ functions/func-style.php
@@ -1,5 +1,6 @@
 <?php
 
 function enqueue_styles() {
-    wp_enqueue_script( 'main', get_template_directory_uri() . '/assets/css/main.css', false, '1.0', 'all' );
+    wp_enqueue_style( 'main', get_template_directory_uri() . '/assets/css/main.css', false, '1.0', 'all' );
+    wp_enqueue_style( 'tailwind', get_template_directory_uri() . '/assets/css/tailwind.css', false, '1.0', 'all' );
 }
